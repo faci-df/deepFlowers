f7758a5a4bc5ec874131e2be7c18e3131efed11b
hyonsokshin <faci_df@hyonsok.io>
2017-04-23 01:22:19 +0900

    App Mock Up Design Finished with main color concept etc.

--- app/src/main/java/io/deepflowers/getflowername/NavActivity.java
+++ app/src/main/java/io/deepflowers/getflowername/NavActivity.java
@@ -80,17 +80,9 @@ public class NavActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        if (id == R.id.nav_list) {
+            // List results after deep search.
+        } else if (id == R.id.nav_memo) {
 
         }
 
