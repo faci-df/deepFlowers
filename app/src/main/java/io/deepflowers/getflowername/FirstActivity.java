708fbf9c4fc1d232e0847004d6f7d5570dee83b4
hyonsokshin <faci_df@hyonsok.io>
2017-04-23 00:22:15 +0900

    Arrange Camera Icons

--- app/src/main/java/io/deepflowers/getflowername/FirstActivity.java
+++ app/src/main/java/io/deepflowers/getflowername/FirstActivity.java
@@ -1,5 +1,7 @@
 package io.deepflowers.getflowername;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(FirstActivity.this,NavActivity.class);
+
+                startActivity(i);
+                finish();
+            }
+        },1000);
+
     }
 }
