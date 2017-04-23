bddfc740a32536a8d0db5ecbfd3dffc6a39c4206
hyonsokshin <faci_df@hyonsok.io>
2017-04-23 19:49:26 +0900

    Changed Init Page & Added image icons.

--- app/src/main/java/io/deepflowers/getflowername/NavActivity.java
+++ app/src/main/java/io/deepflowers/getflowername/NavActivity.java
@@ -1,6 +1,8 @@
 package io.deepflowers.getflowername;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -27,8 +29,13 @@ public class NavActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  //      .setAction("Action", null).show();
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+
+
             }
         });
 
